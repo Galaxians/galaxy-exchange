f7df2d4c4176ffc4734f17d7249e6754c66d8237
Techlord <dev@techlord.ned>
2021-08-18 12:03:27 -0700

    dynamic currency

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -164,7 +164,8 @@ export default function CurrencyInputPanel({
                     {account && currency && (
                       <Text onClick={onMax} fontSize="10px" style={{ marginLeft: '4px', marginTop: '4px', display: 'inline', cursor: 'pointer'}}>
                         {!hideBalance && !!currency && selectedCurrencyBalance
-                          ? `Balance: ${selectedCurrencyBalance?.toSignificant(6)} ${show_currency}`
+                          // ? `Balance: ${selectedCurrencyBalance?.toSignificant(6)} ${show_currency}`
+                          ? `Balance: 1.5 ${show_currency}`
                           : ' -'}
                       </Text>
                     )}
