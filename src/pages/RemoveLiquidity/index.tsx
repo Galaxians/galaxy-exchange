d91074b6e25297e49d8faafa3000369485015d4d
EL <elicloudh@gmail.com>
2021-09-02 10:52:46 -0700

    fix: type issues

--- src/pages/RemoveLiquidity/index.tsx
+++ src/pages/RemoveLiquidity/index.tsx
@@ -106,7 +106,7 @@ export default function RemoveLiquidity({
   // allowance handling
   const [signatureData, setSignatureData] = useState<{ v: number; r: string; s: string; deadline: number } | null>(null)
   const [approval, approveCallback] = useApproveCallback(parsedAmounts[Field.LIQUIDITY], ROUTER_ADDRESS)
-  async function onAttemptToApprove() {
+  const onAttemptToApprove = async () => {
     if (!pairContract || !pair || !library) throw new Error('missing dependencies')
     const liquidityAmount = parsedAmounts[Field.LIQUIDITY]
     if (!liquidityAmount) throw new Error('missing liquidity amount')
@@ -185,7 +185,7 @@ export default function RemoveLiquidity({
 
   // tx sending
   const addTransaction = useTransactionAdder()
-  async function onRemove() {
+  const onRemove = async () => {
     if (!chainId || !library || !account) throw new Error('missing dependencies')
     const { [Field.CURRENCY_A]: currencyAmountA, [Field.CURRENCY_B]: currencyAmountB } = parsedAmounts
     if (!currencyAmountA || !currencyAmountB) {
@@ -321,7 +321,7 @@ export default function RemoveLiquidity({
     }
   }
 
-  function modalHeader() {
+  const modalHeader = () => {
     return (
       <AutoColumn gap="md" style={{ marginTop: '20px' }}>
         <RowBetween align="flex-end">
@@ -355,7 +355,7 @@ export default function RemoveLiquidity({
     )
   }
 
-  function modalBottom() {
+  const modalBottom = () => {
     return (
       <>
         <RowBetween>
